d0bba81db8d056364c1cef47206f9767b2ff2bfe
Brungardt <David.Brungardt@bsbna.com>
2019-09-20 13:08:08 -0500

    Added some functionality to UI.

--- AngleBracingPlugin/Form1.Designer.cs
+++ AngleBracingPlugin/Form1.Designer.cs
@@ -51,9 +51,10 @@
             this.structuresExtender.SetAttributeTypeName(this.okApplyModifyGetOnOffCancel1, null);
             this.structuresExtender.SetBindPropertyName(this.okApplyModifyGetOnOffCancel1, null);
             this.okApplyModifyGetOnOffCancel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.okApplyModifyGetOnOffCancel1.Location = new System.Drawing.Point(0, 337);
+            this.okApplyModifyGetOnOffCancel1.Location = new System.Drawing.Point(0, 414);
+            this.okApplyModifyGetOnOffCancel1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.okApplyModifyGetOnOffCancel1.Name = "okApplyModifyGetOnOffCancel1";
-            this.okApplyModifyGetOnOffCancel1.Size = new System.Drawing.Size(634, 29);
+            this.okApplyModifyGetOnOffCancel1.Size = new System.Drawing.Size(845, 36);
             this.okApplyModifyGetOnOffCancel1.TabIndex = 0;
             // 
             // saveLoad1
@@ -67,9 +68,10 @@
             this.saveLoad1.HelpKeyword = "";
             this.saveLoad1.HelpUrl = "";
             this.saveLoad1.Location = new System.Drawing.Point(0, 0);
+            this.saveLoad1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.saveLoad1.Name = "saveLoad1";
             this.saveLoad1.SaveAsText = "";
-            this.saveLoad1.Size = new System.Drawing.Size(634, 43);
+            this.saveLoad1.Size = new System.Drawing.Size(845, 53);
             this.saveLoad1.TabIndex = 1;
             this.saveLoad1.UserDefinedHelpFilePath = null;
             // 
@@ -79,10 +81,9 @@
             this.structuresExtender.SetAttributeTypeName(this.lbl_AngleBracingType, null);
             this.lbl_AngleBracingType.AutoSize = true;
             this.structuresExtender.SetBindPropertyName(this.lbl_AngleBracingType, null);
-            this.lbl_AngleBracingType.Location = new System.Drawing.Point(9, 46);
-            this.lbl_AngleBracingType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_AngleBracingType.Location = new System.Drawing.Point(12, 57);
             this.lbl_AngleBracingType.Name = "lbl_AngleBracingType";
-            this.lbl_AngleBracingType.Size = new System.Drawing.Size(103, 13);
+            this.lbl_AngleBracingType.Size = new System.Drawing.Size(136, 17);
             this.lbl_AngleBracingType.TabIndex = 2;
             this.lbl_AngleBracingType.Text = "Angle Bracing Type:";
             // 
@@ -95,11 +96,12 @@
             this.AngleBracingType.Items.AddRange(new object[] {
             "Single",
             "Double"});
-            this.AngleBracingType.Location = new System.Drawing.Point(116, 44);
-            this.AngleBracingType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AngleBracingType.Location = new System.Drawing.Point(155, 54);
+            this.AngleBracingType.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AngleBracingType.Name = "AngleBracingType";
-            this.AngleBracingType.Size = new System.Drawing.Size(92, 21);
+            this.AngleBracingType.Size = new System.Drawing.Size(121, 24);
             this.AngleBracingType.TabIndex = 3;
+            this.AngleBracingType.SelectedIndexChanged += new System.EventHandler(this.AngleBracingType_SelectedIndexChanged);
             // 
             // lbl_AngleProfile
             // 
@@ -107,9 +109,10 @@
             this.structuresExtender.SetAttributeTypeName(this.lbl_AngleProfile, null);
             this.lbl_AngleProfile.AutoSize = true;
             this.structuresExtender.SetBindPropertyName(this.lbl_AngleProfile, null);
-            this.lbl_AngleProfile.Location = new System.Drawing.Point(213, 46);
+            this.lbl_AngleProfile.Location = new System.Drawing.Point(284, 57);
+            this.lbl_AngleProfile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_AngleProfile.Name = "lbl_AngleProfile";
-            this.lbl_AngleProfile.Size = new System.Drawing.Size(72, 13);
+            this.lbl_AngleProfile.Size = new System.Drawing.Size(96, 17);
             this.lbl_AngleProfile.TabIndex = 4;
             this.lbl_AngleProfile.Text = "Angle Profile: ";
             // 
@@ -123,10 +126,12 @@
             "L3X3X1/4",
             "L4X3X3/8",
             "L5X5X1/2"});
-            this.AngleBracingProfile.Location = new System.Drawing.Point(291, 44);
+            this.AngleBracingProfile.Location = new System.Drawing.Point(388, 54);
+            this.AngleBracingProfile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AngleBracingProfile.Name = "AngleBracingProfile";
-            this.AngleBracingProfile.Size = new System.Drawing.Size(121, 21);
+            this.AngleBracingProfile.Size = new System.Drawing.Size(160, 24);
             this.AngleBracingProfile.TabIndex = 5;
+            this.AngleBracingProfile.SelectedIndexChanged += new System.EventHandler(this.AngleBracingProfile_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
@@ -134,9 +139,10 @@
             this.structuresExtender.SetAttributeTypeName(this.pictureBox1, null);
             this.structuresExtender.SetBindPropertyName(this.pictureBox1, null);
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(6, 104);
+            this.pictureBox1.Location = new System.Drawing.Point(8, 128);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(305, 227);
+            this.pictureBox1.Size = new System.Drawing.Size(407, 279);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -147,9 +153,10 @@
             this.structuresExtender.SetAttributeTypeName(this.pictureBox2, null);
             this.structuresExtender.SetBindPropertyName(this.pictureBox2, null);
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(317, 104);
+            this.pictureBox2.Location = new System.Drawing.Point(423, 128);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(306, 227);
+            this.pictureBox2.Size = new System.Drawing.Size(408, 279);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -160,9 +167,10 @@
             this.structuresExtender.SetAttributeTypeName(this.lbl_AnglePosition, null);
             this.lbl_AnglePosition.AutoSize = true;
             this.structuresExtender.SetBindPropertyName(this.lbl_AnglePosition, null);
-            this.lbl_AnglePosition.Location = new System.Drawing.Point(419, 50);
+            this.lbl_AnglePosition.Location = new System.Drawing.Point(559, 62);
+            this.lbl_AnglePosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_AnglePosition.Name = "lbl_AnglePosition";
-            this.lbl_AnglePosition.Size = new System.Drawing.Size(77, 13);
+            this.lbl_AnglePosition.Size = new System.Drawing.Size(102, 17);
             this.lbl_AnglePosition.TabIndex = 8;
             this.lbl_AnglePosition.Text = "Angle Position:";
             // 
@@ -175,20 +183,25 @@
             this.AnglePosition.Items.AddRange(new object[] {
             "Centered",
             "Offset"});
-            this.AnglePosition.Location = new System.Drawing.Point(501, 47);
+            this.AnglePosition.Location = new System.Drawing.Point(668, 58);
+            this.AnglePosition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AnglePosition.Name = "AnglePosition";
-            this.AnglePosition.Size = new System.Drawing.Size(121, 21);
+            this.AnglePosition.Size = new System.Drawing.Size(160, 24);
             this.AnglePosition.TabIndex = 9;
+            this.AnglePosition.SelectedIndexChanged += new System.EventHandler(this.AnglePosition_SelectedIndexChanged);
             // 
             // AngleOffset
             // 
             this.structuresExtender.SetAttributeName(this.AngleOffset, "AngleOffset");
             this.structuresExtender.SetAttributeTypeName(this.AngleOffset, "Distance");
             this.structuresExtender.SetBindPropertyName(this.AngleOffset, null);
-            this.AngleOffset.Location = new System.Drawing.Point(86, 75);
+            this.AngleOffset.Location = new System.Drawing.Point(115, 92);
+            this.AngleOffset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AngleOffset.Name = "AngleOffset";
-            this.AngleOffset.Size = new System.Drawing.Size(100, 20);
+            this.AngleOffset.Size = new System.Drawing.Size(132, 22);
             this.AngleOffset.TabIndex = 10;
+            this.AngleOffset.Visible = false;
+            this.AngleOffset.TextChanged += new System.EventHandler(this.AngleOffset_TextChanged);
             // 
             // lbl_AngleOffset
             // 
@@ -196,20 +209,22 @@
             this.structuresExtender.SetAttributeTypeName(this.lbl_AngleOffset, null);
             this.lbl_AngleOffset.AutoSize = true;
             this.structuresExtender.SetBindPropertyName(this.lbl_AngleOffset, null);
-            this.lbl_AngleOffset.Location = new System.Drawing.Point(12, 78);
+            this.lbl_AngleOffset.Location = new System.Drawing.Point(16, 96);
+            this.lbl_AngleOffset.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_AngleOffset.Name = "lbl_AngleOffset";
-            this.lbl_AngleOffset.Size = new System.Drawing.Size(68, 13);
+            this.lbl_AngleOffset.Size = new System.Drawing.Size(90, 17);
             this.lbl_AngleOffset.TabIndex = 11;
             this.lbl_AngleOffset.Text = "Angle Offset:";
+            this.lbl_AngleOffset.Visible = false;
             // 
             // AngleBraceFrm
             // 
             this.structuresExtender.SetAttributeName(this, null);
             this.structuresExtender.SetAttributeTypeName(this, null);
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.structuresExtender.SetBindPropertyName(this, null);
-            this.ClientSize = new System.Drawing.Size(634, 366);
+            this.ClientSize = new System.Drawing.Size(845, 450);
             this.Controls.Add(this.lbl_AngleOffset);
             this.Controls.Add(this.AngleOffset);
             this.Controls.Add(this.AnglePosition);
@@ -222,7 +237,7 @@
             this.Controls.Add(this.lbl_AngleBracingType);
             this.Controls.Add(this.saveLoad1);
             this.Controls.Add(this.okApplyModifyGetOnOffCancel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AngleBraceFrm";
             this.Text = "Angle Bracing";
             this.Load += new System.EventHandler(this.Form1_Load);
--- AngleBracingPlugin/Form1.cs
+++ AngleBracingPlugin/Form1.cs
@@ -16,6 +16,7 @@ using T3D = Tekla.Structures.Geometry3d;
 using TSMUI = Tekla.Structures.Model.UI;
 using Tekla.Structures.Plugins;
 
+
 namespace AngleBracingPlugin
 {
 
@@ -65,5 +66,101 @@ namespace AngleBracingPlugin
             this.ToggleSelection();
         }
         #endregion
+        
+        // Combobox for setting angle bracing connection to single angle or double angle
+        private void AngleBracingType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (AngleBracingType.SelectedIndex)
+            {
+                // If single angle bracing
+                case 0: SetAttributeValue(AngleBracingType, 0);
+                break;
+                // If double angle bracing
+                case 1:
+                    SetAttributeValue(AngleBracingType, 1);
+                    break;
+                // Default to single angle bracing
+                default:
+                    SetAttributeValue(AngleBracingType, 0);
+                    break;
+            }
+        }
+
+        // Combobox for setting angle profile
+        private void AngleBracingProfile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (AngleBracingProfile.SelectedIndex)
+            {
+                // Three options for angle profile within plugin
+
+                // 0 = L3X3X1/4 
+                case 0:
+                    SetAttributeValue(AngleBracingProfile, 0);
+                    break;
+                // 1 = L4X4X3/8
+                case 1:
+                    SetAttributeValue(AngleBracingProfile, 1);
+                    break;
+                // 2 = L5X5X1/2 
+                case 2:
+                    SetAttributeValue(AngleBracingProfile, 2);
+                    break;
+                // default to 0 = L3X3X1/4 
+                default:
+                    SetAttributeValue(AngleBracingProfile, 0);
+                    break;
+            }
+        }
+
+        // Combobox for setting angle to centered or offset
+        private void AnglePosition_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (AnglePosition.SelectedIndex)
+            {
+                // 0 = Angle is centered
+                case 0:
+                    SetAttributeValue(AnglePosition, 0);
+                    // Hide option for angle offset
+                    lbl_AngleOffset.Visible = false;
+                    AngleOffset.Visible = false;
+                    break;
+                // 0 = Angle is offset
+                case 1:
+                    SetAttributeValue(AnglePosition, 1);
+                    // Show option for angle offset
+                    lbl_AngleOffset.Visible = true;
+                    AngleOffset.Visible = true;
+                    break;
+                // default to centered
+                default:
+                    SetAttributeValue(AnglePosition, 0);
+                    // Hide option for angle offset
+                    lbl_AngleOffset.Visible = false;
+                    AngleOffset.Visible = false;
+                    break;
+            }
+        }
+
+        // Textbox to set distance for angle offset if that option is chosen
+        private void AngleOffset_TextChanged(object sender, EventArgs e)
+        {
+            double angleOffset;
+            bool offsetEntered;
+
+            // Check to see if AngleOffset value is null or 0
+            offsetEntered = Double.TryParse(AngleOffset.Text, out angleOffset);
+
+            // If conversion was successful and value is greater than 0
+            if (offsetEntered && angleOffset != 0.0)
+            {
+                // set attribute value to value entered by user
+                SetAttributeValue(AngleOffset, angleOffset);
+            }
+            else
+            {
+                // default value to -1 to leave the angle centered.
+                SetAttributeValue(AngleOffset, -1);
+            }
+        }
     }
 }
